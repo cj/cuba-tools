8d46886ce722ea8df9cbc1af2cf5945df5d19ce8
cj <cjlazell@gmail.com>
2014-05-24 00:25:06 -0500

    fix to_deep_ostruct

--- cuba-tools.gemspec
+++ cuba-tools.gemspec
@@ -4,7 +4,7 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 Gem::Specification.new do |spec|
   spec.name          = "cuba-tools"
-  spec.version       = "0.0.6"
+  spec.version       = "0.0.7"
   spec.authors       = ["cj"]
   spec.email         = ["cjlazell@gmail.com"]
   spec.description   = %q{Contains a group of tools to extend cuba}
--- lib/cuba/tools/inflectors.rb
+++ lib/cuba/tools/inflectors.rb
@@ -22,26 +22,27 @@ class Object
 end
 
 class DeepOpenStruct < OpenStruct
-  def initialize(hash=nil)
+  def initialize hash = nil
 
     @table = {}
     @hash_table = {}
 
+    hash = [hash] unless hash.is_a? Hash
+
     if hash
       hash.each do |k,v|
 
-        if v.is_a?(Array)
-          other = Array.new()
+        if v.is_a? Array
+          @table[k.to_sym] ||= []
+
           v.each { | entry |
-            other.push(self.class.new(entry))
+            @table[k.to_sym] << entry
           }
-          v = other
+        else
+          @table[k.to_sym] = (v.is_a?(Hash) ? self.class.new(v) : v)
+          @hash_table[k.to_sym] = v
+          new_ostruct_member(k)
         end
-
-        @table[k.to_sym] = (v.is_a?(Hash) ? self.class.new(v) : v)
-        @hash_table[k.to_sym] = v
-        new_ostruct_member(k)
-
       end
     end
   end
